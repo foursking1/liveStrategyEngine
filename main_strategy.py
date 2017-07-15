2edf68ecb96f63af8796a5760c5bc16ab78855d1
foursking <foursking@gmail.com>
2017-07-15 10:52:44 +0800

    rua

--- main_strategy.py
+++ main_strategy.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-import userStrategy.Compare as Compare
+import userStrategy.firstTest as firstTest
 from liveStrategyEngine.BaseMultipleStartegyEngine import BaseMultiLiveStrategyEngine
 from exchange.HuobiExchange import HuobiExchange
 from utils.helper import *
@@ -11,6 +11,6 @@ from utils.helper import *
 if __name__ == "__main__":
 
     exchanges = [HuobiExchange]
-    strat = BaseMultiLiveStrategyEngine(Compare, exchanges, 1, datetime.datetime.now(), 0.1, 30,
+    strat = BaseMultiLiveStrategyEngine(firstTest, exchanges, 1, datetime.datetime.now(), 0.1, 30,
                                         dailyExitTime="23:30:00")
     start_strat(strat)
