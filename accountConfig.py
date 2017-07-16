3aba789c5a2eaf1aac75e0c371eb99289bfc0040
foursking <foursking@gmail.com>
2017-07-16 23:14:32 +0800

    rua

--- accountConfig.py
+++ accountConfig.py
@@ -5,8 +5,8 @@
 HUOBI = {
     "CNY_1":
         {
-            "ACCESS_KEY": "",
-            "SECRET_KEY": "",
+            "ACCESS_KEY": "8326e2b5-15bac5d1-edf76729-ae618",
+            "SECRET_KEY": "9ef30aeb-3a052184-5267daf1-2b0d1",
             "SERVICE_API": "https://api.huobi.com/apiv3",
         },
     "USD_1":
@@ -32,8 +32,8 @@ BITVC = {
 OKCOIN = {
     "CNY_1":
         {
-            "ACCESS_KEY": "",
-            "SECRET_KEY": "",
+            "ACCESS_KEY": "f1686545-1012-400e-8ddc-1c0dfeea1abe",
+            "SECRET_KEY": "6A1A054D0978788F132DDDFDFB94A439",
             "SERVICE_API": "https://www.okcoin.cn",  # okcoin国内站
         },
 
